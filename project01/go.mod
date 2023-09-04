cce331e7abd3792b12560153f00bf878f23198a3
AprilXS <67018736+AprilXS@users.noreply.github.com>
2023-09-04 07:40:32 +0000

    still the problem with the binding json, I will check it later, also I delete the go.mod from the global Go folder, and make every project its own go.mod

---
+++ project01/go.mod
@@ -0,0 +1,3 @@
+module example/project01
+
+go 1.21.0
--- project02/main.go
+++ project02/main.go
@@ -9,18 +9,26 @@ import (
 	//"errors" // Error handling
 )
 
+// type book struct {
+// 	ID       string `json:"id"`
+// 	Title    string `json:"title"`
+// 	Author   string `json:"author"`
+// 	Quantity int    `json:"quantity"`
+// } // Book struct
+
 type book struct {
 	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Author   string `json:"author"`
-	Quantity int    `json:"quantity"`
-} // Book struct
+}
+
+// var books = []book{
+// 	{ID: "1", Title: "The Hitchhiker's Guide to the Galaxy", Author: "Douglas Adams", Quantity: 10},
+// 	{ID: "2", Title: "Cloud Native Go", Author: "M.-L. Reimer", Quantity: 5},
+// 	{ID: "3", Title: "The Starfield", Author: "Bathezda", Quantity: 10},
+// } // Array of books
 
 var books = []book{
-	{ID: "1", Title: "The Hitchhiker's Guide to the Galaxy", Author: "Douglas Adams", Quantity: 10},
-	{ID: "2", Title: "Cloud Native Go", Author: "M.-L. Reimer", Quantity: 5},
-	{ID: "3", Title: "The Starfield", Author: "Bathezda", Quantity: 10},
-} // Array of books
+	{ID: "1"},
+}
 
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books) // 200 OK + JSON body of books array
@@ -38,6 +46,27 @@ func createBook(c *gin.Context) {
 	books = append(books, newBook)
 	c.IndentedJSON(http.StatusCreated, newBook) // 201 Created + JSON body of newBook
 } // createBook function
+
+// func createBook(c *gin.Context) {
+// 	var newBook book
+
+// 	if err := c.BindJSON(&newBook); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+
+// 	// Validate the new book data (you can add more validation rules)
+// 	if newBook.Title == "" || newBook.Author == "" || newBook.Quantity <= 0 {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid book data"})
+// 		return
+// 	}
+
+// 	// Assuming 'books' is a slice of book
+// 	books = append(books, newBook)
+// 	c.JSON(http.StatusCreated, gin.H{"message": "Book created successfully", "data": newBook})
+// }
+
 
 func createboook(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, gin.H{"message": "A new book has been created."})
